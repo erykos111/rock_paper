b8b22a3420a76002c1b4ef2329a96a35fa036bab
erykos111 <erykos111@gmail.com>
2022-04-01 23:12:54 +0100

    Add initial game logic

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function random_number(){
-    const random_number = ((Math.random() * 6) + 1)
-    console.log(random_number)
+const allowed_moves = ['rock','paper','scissors']
+
+function computer_move(){
+    const random_number = Math.trunc((Math.random() * 3))
+    computer_moves = allowed_moves[random_number]
+    
 }
 
 
-
-function chuj(){
-  console.log((4+6+9)/77)
-
+function player_move(move){
+  player_moves = move.toLowerCase()
+  if(allowed_moves.includes(player_moves)){
+    console.log(player_moves)
+    check_result(player_moves, computer_moves)
+  }
+  else{
+    console.log("Wrong move, try again")
+  }
 }
+
+function check_result(player_moves, computer_moves){
+  console.log("Computer Moves " + computer_moves)
+  if (player_moves == computer_moves){
+    console.log("Draw")
+  }
+  else if (player_moves == 'rock' && computer_moves == 'scissors' || player_moves == 'paper' && computer_moves == "rock" || player_moves == "scissors" && computer_moves == "paper" ){
+    console.log("Player wins")
+  }
+  else{
+    console.log("Computer wins")
+  }
+  }
+  
